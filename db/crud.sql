d11e6c266618c20fdc400b9c1868017f25e68afc
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-09-15 19:16:44 -0300

    refact

--- db/crud.sql
+++ db/crud.sql
@@ -1,3 +1,8 @@
 CREATE DATABASE crud;
 
-CREATE TABLE `crud`.`cadastro` (`id` INT NOT NULL AUTO_INCREMENT , `Nome` VARCHAR(90) NOT NULL , `Sexo` VARCHAR(30) NOT NULL , `Cidade` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `crud`.`cadastro` (
+    `id` INT NOT NULL AUTO_INCREMENT , 
+    `Nome` VARCHAR(90) NOT NULL , 
+    `Sexo` VARCHAR(30) NOT NULL , 
+    `Cidade` VARCHAR(50) NOT NULL , 
+    PRIMARY KEY (`id`)) ENGINE = InnoDB;
