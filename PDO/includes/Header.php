c79f6fbe1b112835fd7c6538b72fcf4ec904c10a
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-09-15 19:47:33 -0300

    Preparando o layout da página e add includes

---
+++ PDO/includes/Header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class Crud</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="banner">
+        <img src="images/572552750819-crud-php.jpg" alt="banner com o nome crud">
+    </div>
+    <div class="nav">
+        <li><a href="cadastro">Cadastro</a></li>
+        <li><a href="selecao">Seleção</a></li>
+        <li><a href="delete">Delete</a></li>
+        <li><a href="atualizacao">Atualização</a></li>
+    </div>
--- PDO/index.php
+++ PDO/index.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Class Crud</title>
-    <?php include 'Class/ClassConexao.php' ;?>
-</head>
-<body>
+<?php include_once 'includes/Header.php' ;?>
 
-</body>
-</html>
+    <div class="content">
+        <?php include 'Class/ClassConexao.php' ;?>
+
+    </div>
+    
+<?php include_once 'includes/Footer.php' ;?>
