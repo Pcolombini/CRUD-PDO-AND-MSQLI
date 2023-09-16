671f96b62cd53caa5d3bb1c956fa9a2bc67e9739
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-09-16 14:40:22 -0300

    add fet and refacts

--- PDO/cadastro.php
+++ PDO/cadastro.php
@@ -11,31 +11,32 @@
     <div class="formulario">
 
         <h1 class="center">Cadastro</h1>
-
-        <div class="formularioInput">
-
+        <form name="fomrCadastro" id="fomrCadastro" method="post" action="controller/ControllerCadastro.php">
             <div class="formularioInput">
-                Nome:<br>
-                <input type="text" name="nome" id="nome">
-            </div>
-            
 
-            <div class="formularioInput">
-                Sexo:<br>
-                <input type="text" name="sexo" id="sexo">
-            </div>
-            
+                <div class="formularioInput">
+                    Nome:<br>
+                    <input type="text" name="nome" id="nome">
+                </div>
 
-            <div class="formularioInput">
-                Cidade:<br>
-                <input type="text" name="cidade" id="cidade">
-            </div>
-            
 
-            <div class="formularioInput formularioInput100 center">
-                <input type="submit" value="Cadastrar">
+                <div class="formularioInput">
+                    Sexo:<br>
+                    <input type="text" name="sexo" id="sexo">
+                </div>
+
+
+                <div class="formularioInput">
+                    Cidade:<br>
+                    <input type="text" name="cidade" id="cidade">
+                </div>
+
+
+                <div class="formularioInput formularioInput100 center">
+                    <input type="submit" value="Cadastrar">
+                </div>
             </div>
-        </div>
+        </form>
     </div>
 
 </div>
