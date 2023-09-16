8c3a821420eecd710403a5c452973aa521ce055d
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-09-16 14:45:48 -0300

    refact

--- PDO/includes/Header.php
+++ PDO/includes/Header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Class Crud</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="banner">
         <img src="images/572552750819-crud-php.jpg" alt="banner com o nome crud">
@@ -16,3 +18,4 @@
         <li><a href="delete">Delete</a></li>
         <li><a href="atualizacao">Atualização</a></li>
     </div>
+    
